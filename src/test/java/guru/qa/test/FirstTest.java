4f7bdcb25b2bcb63f7fb1e393b89356eb053dc31
Lisa <bear.bessie@gmail.com>
2021-12-02 11:33:34 +0300

    DZ 10 - Добавлен  Selenoid support

--- src/test/java/guru/qa/test/FirstTest.java
+++ src/test/java/guru/qa/test/FirstTest.java
@@ -1,10 +1,12 @@
 package guru.qa.test;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.data.RegistrationData;
 import guru.qa.pages.RegistrationPage;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -13,6 +15,11 @@ public class FirstTest extends BaseTest {
     @Test
     void firstTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
 
         RegistrationPage registrationPage = new RegistrationPage(new RegistrationData());
 
