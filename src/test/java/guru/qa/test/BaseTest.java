1622e3bd44119c0c33c219c46f14b781e677b0b7
Lisa <bear.bessie@gmail.com>
2021-12-03 10:38:37 +0300

    DZ 10 - Добавлены Attachments (видео, скрин, page sourse, логи консоли браузера)

--- src/test/java/guru/qa/test/BaseTest.java
+++ src/test/java/guru/qa/test/BaseTest.java
@@ -1,12 +1,39 @@
 package guru.qa.test;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import guru.qa.util.Attachments;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
+
         Configuration.browserSize = "1900x1200";
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        //_______Selenoid Usage__________________________________________________________
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        //_______________________________________________________________________________
+    }
+
+    @AfterEach
+    public void afterEachAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
     }
 }
+
--- src/test/java/guru/qa/test/FirstTest.java
+++ src/test/java/guru/qa/test/FirstTest.java
@@ -1,25 +1,14 @@
 package guru.qa.test;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.data.RegistrationData;
 import guru.qa.pages.RegistrationPage;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Selenide.*;
 
 public class FirstTest extends BaseTest {
 
     @Test
     void firstTest() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
 
         RegistrationPage registrationPage = new RegistrationPage(new RegistrationData());
 
@@ -27,9 +16,6 @@ public class FirstTest extends BaseTest {
                 .fillEntireForm()
                 .pressSubmit();
         registrationPage.checkResultTable();
-
-        sleep(10000);
-
     }
 
 }
